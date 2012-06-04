adc828654b33f09951b339351df3087b61c6565b
starkingwx <starkingwx@gmail.com>
2012-06-04 11:21:23 +0800

    Initial Commit

---
+++ imeeting_iphone/ECAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ECAppDelegate.h
+//  imeeting_iphone
+//
+//  Created by star king on 12-6-4.
+//  Copyright (c) 2012年 elegant cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ imeeting_iphone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  imeeting_iphone
+//
+//  Created by star king on 12-6-4.
+//  Copyright (c) 2012年 elegant cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ECAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ECAppDelegate class]));
+    }
+}
