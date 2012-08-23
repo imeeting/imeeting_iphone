523cc6d636bb502795507b47c355660a9d5bdd13
starkingwx <starkingwx@gmail.com>
2012-08-23 15:08:31 +0800

    add App Util

---
+++ imeeting_iphone/Util/ECAppUtil.h
@@ -0,0 +1,15 @@
+//
+//  ECAppUtil.h
+//  imeeting_iphone
+//
+//  Created by king star on 12-8-23.
+//  Copyright (c) 2012年 elegant cloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ECAppUtil : NSObject
+
++ (void)addCallCenterNumberToAddressBook;
+
+@end
---
+++ imeeting_iphone/Util/ECAppUtil.m
@@ -0,0 +1,19 @@
+//
+//  ECAppUtil.m
+//  imeeting_iphone
+//
+//  Created by king star on 12-8-23.
+//  Copyright (c) 2012年 elegant cloud. All rights reserved.
+//
+
+#import "ECAppUtil.h"
+#import "ECConstants.h"
+
+@implementation ECAppUtil
+
++ (void)addCallCenterNumberToAddressBook {
+    NSString *name = NSLocalizedString(@"call center", nil);
+    NSString *number = CALL_CENTER_NUM;
+    
+}
+@end
